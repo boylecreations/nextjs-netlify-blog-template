c3a3a75c5cf6fa8e35f4c56504440c361b057aac
Aidan Boyle <aidan@boylecreations.com>
2021-04-20 05:06:40 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,11 @@ export default function Index() {
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
 
-<section class="text-gray-600 body-font relative">
+
+
+      <div className="container">
+        <div>
+        <section class="text-gray-600 body-font relative">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-12">
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
@@ -73,9 +77,6 @@ export default function Index() {
     </div>
   </div>
 </section>
-GitHub
-      <div className="container">
-        <div>
           <h1>
             Hi, We're Next.js  Netlify - How Amazing, lets add Tailwind now.<span className="fancy">.</span>
           </h1>
